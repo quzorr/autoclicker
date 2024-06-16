7c306cf443c68e0afb3d80e526823da713b798c0
quzor <egor.khantakov@gmail.com>
2024-06-16 18:04:17 +0700

    .

--- autoclicker.py
+++ autoclicker.py
@@ -2,7 +2,7 @@ import time
 import keyboard
 import pyautogui
 
-MAX_WORK_TIME = 10 * 3
+MAX_WORK_TIME = 10 * 4
 
 work = False
 
@@ -21,10 +21,11 @@ def main():
 
     while True:
         difference = time.time() - start_time
+
         if work and difference < MAX_WORK_TIME:
             pyautogui.click()
             count_clicks += 1
-            print(f'(CLICKING) click number: {count_clicks} {" "*2} time: {round(difference, 2)}')
+            print(f'(CLICKING) click number: {count_clicks} {" " * 2} time: {round(difference, 2)}')
 
         elif difference < MAX_WORK_TIME:
             print(f'(STOP) time: {round(difference, 2)}')
